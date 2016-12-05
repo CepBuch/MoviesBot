20004490f6613b0c2624a4c30da2b523b18fd399
Сергей <@CEPBUCH-PC>
2016-12-05 15:29:25 +0300

    Added MovieDTO folder and divided DTOs. New class TelegramBotClient

---
+++ MoviesBot/MoviesBot/TelegramBotClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesBot
+{
+    class TelegramBotClient
+    {
+        private readonly string _token;
+        private const string _baseUrl = "https://api.telegram.org/bot";
+
+        public TelegramBotClient(string token)
+        {
+            _token = token;
+        }
+    }
+}
