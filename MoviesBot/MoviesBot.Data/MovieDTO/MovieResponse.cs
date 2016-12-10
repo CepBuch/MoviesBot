0e3a6f9720675230243fa5dea1e98e4827bfc50b
lena1998lena <lena.tyuhova@yandex.ru>
2016-12-10 20:27:09 +0300

    Added MovieDTO model

---
+++ MoviesBot/MoviesBot.Data/MovieDTO/MovieResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesBot.Data.MovieDTO
+{
+    class MovieResponse
+    {
+        [JsonProperty(PropertyName = "Search")]
+        public List<Movie> Movies { get; set; }
+        [JsonProperty(PropertyName = "totalResults")]
+        public int TotalResults { get; set; }
+
+    }
+}
